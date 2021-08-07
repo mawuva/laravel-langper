2d47acbbb1022bad1892cc452e1a14eece7ab77d
Ephraïm SEDDOR <seddorephraim7@gmail.com>
2021-08-08 00:24:16 +0100

    Update LangperServiceProvider.php

--- src/LangperServiceProvider.php
+++ src/LangperServiceProvider.php
@@ -50,7 +50,7 @@ class LangperServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'langper');
+        //$this->mergeConfigFrom(__DIR__.'/../config/config.php', 'langper');
 
         // Register the main class to use with the facade
         $this->app->singleton('langper', function () {
